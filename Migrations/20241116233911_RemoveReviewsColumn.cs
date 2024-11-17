c01f46e14e525f6fc1109189bcf8f18ac3860e07
ghassen <ghassensoua@gmail.com>
2024-11-16 19:04:21 -0500

    remove anused column

---
+++ Migrations/20241116233911_RemoveReviewsColumn.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReview.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveReviewsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviewers_PokemonId",
+                table: "Reviewers"
+                );
+            // Drop foreign key
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviewers_Pokemons_PokemonId",
+                table: "Reviewers"
+                );
+            migrationBuilder.DropColumn(
+                name: "PokemonId",
+                table: "Reviewers"
+                );
+    }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
